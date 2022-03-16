b4434e416ddbda6988261e651d17c06470b06913
DavitGasparyan06 <davit.gasparyan6.y@tumo.org>
2022-03-16 19:55:27 +0400

    test

--- class.js
+++ class.js
@@ -7,7 +7,7 @@ class LivingCreature {
             [this.x - 1, this.y - 1],
             [this.x, this.y - 1],
             [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
+            [this.x - 1, this.y], 
             [this.x + 1, this.y],
             [this.x - 1, this.y + 1],
             [this.x, this.y + 1],
